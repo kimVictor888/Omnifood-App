5734e8a44d50047b60a3fac04e6db0b1030fe768
Kim Victor <kim.victor.aleksandrovich@gmail.com>
2020-10-12 23:51:14 +0600

    add smooth scroll on links and add scroll on buttons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,33 @@
 $(() => {
+
+    // For sticky navigation
     $('.js--section-features').waypoint(function (direction) {
         if (direction === 'down') {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
+    });
+
+    // Scroll on buttons
+    $('.js--scroll-to-plans').on('click', () => {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').on('click', () => {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+    // Navigation scroll
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
     });
 });
